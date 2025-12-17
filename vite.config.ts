a570c3937dfbc24142d8d77b1e66d8fbfe59d3ae
Ana Thereza Motta <mottaanathereza@gmail.com>
2025-12-17 01:10:09 -0300

    alterei vite e app

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/product-portfolio-builder/", // 👈 IMPORTANTE
+  base: "/product-portfolio-builder/", 
   server: {
     host: "::",
     port: 8080,
